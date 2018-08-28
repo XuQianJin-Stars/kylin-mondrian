cc7e72e1d0fb56f1972c830972f10edc92a42f9f
lwz9103 <lwz9103@163.com>
2018-08-28 14:03:25 +0800

    uncomment properties process

--- src/main/java/mondrian/olap4j/MondrianOlap4jCellSetAxisMetaData.java
+++ src/main/java/mondrian/olap4j/MondrianOlap4jCellSetAxisMetaData.java
@@ -52,7 +52,6 @@ class MondrianOlap4jCellSetAxisMetaData implements CellSetAxisMetaData {
         this.cellSetMetaData = cellSetMetaData;
 
         // populate property list
-        /*
         for (Id id : queryAxis.getDimensionProperties()) {
             final String[] names = id.toStringArray();
             Property olap4jProperty = null;
@@ -78,7 +77,6 @@ class MondrianOlap4jCellSetAxisMetaData implements CellSetAxisMetaData {
             }
             propertyList.add(olap4jProperty);
         }
-        */
     }
 
     public Axis getAxisOrdinal() {
